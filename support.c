cdaaaf1a701e3fa2c31fb72157d581902bbeb430
Žygimantas Marma <zygimantas.marma@nanoavionics.com>
2023-05-06 00:27:24 +0300

    feat: add mqtt support

--- support.c
+++ support.c
@@ -10,6 +10,63 @@
 #include "nrf24.h"
 
 
+
+
+// TODO: good developer would move this to separate file
+//////////////////////////////////////////////////////////////////////////////
+
+#include "MQTTClient.h"
+#define ADDRESS		"mqtt3.thingspeak.com:1883"
+
+#define CLIENTID	"mwa0000018251941"
+#define TOPIC		"channels/2130012/publish"
+#define QOS			0
+#define TIMEOUT		20 * 10000L
+volatile MQTTClient_deliveryToken deliveredToken;
+
+
+#define SECRET_MQTT_USERNAME "HAMXHAY3CgQOBywcBSkFBjM"
+#define SECRET_MQTT_CLIENT_ID "HAMXHAY3CgQOBywcBSkFBjM"
+#define SECRET_MOTT_PASSWORD "zQjHmPsXQk5q1gMBTfknHgiU"
+
+// char payload_string [50]="field1=15.1&field2=10.5";
+char payload_string [50]="";
+
+int MQTT_publish (char *string) {
+	printf("\r\n");
+	printf("String will be published \r\n");
+	printf("%s", string);
+	printf("\r\n");
+	MQTTClient client;
+	MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
+	MQTTClient_message pubmsg =	MQTTClient_message_initializer;
+	MQTTClient_deliveryToken token;
+	MQTTClient_create (&client, ADDRESS, SECRET_MQTT_CLIENT_ID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+	opts.keepAliveInterval = 20;
+	opts.cleansession= 1;
+	opts.username=SECRET_MQTT_CLIENT_ID;
+	opts.password=SECRET_MOTT_PASSWORD;
+
+	int rc;
+	if ((rc =MQTTClient_connect (client, &opts)) != MQTTCLIENT_SUCCESS) {
+		printf("Failed to connect, return code %d /r/n", rc);
+		return -1;
+	}
+
+	pubmsg.payload= string;
+	pubmsg.payloadlen = strlen(string);
+	pubmsg.qos = QOS;
+	pubmsg.retained = 0;
+	MQTTClient_publishMessage (client, TOPIC, &pubmsg, &token);
+	rc = MQTTClient_waitForCompletion (client, token, TIMEOUT);
+	printf("Message with token %d delivered \r\n", (int)token);
+	MQTTClient_disconnect (client, 10000);
+	MQTTClient_destroy (&client);
+	return rc;
+}
+//////////////////////////////////////////////////////////////////////////////
+
+
 // send buffer and receive buffer
 uint32_t data_to_send[10];
 uint32_t data_received[10];
@@ -354,6 +411,9 @@ void RX_single(void)
         UART_SendStr(" MQTT DATA:>");
         UART_SendStr(mqtt_data);
         UART_SendStr("<\r\n");
+
+		MQTT_publish(mqtt_data);
+
       }
     }
   }
